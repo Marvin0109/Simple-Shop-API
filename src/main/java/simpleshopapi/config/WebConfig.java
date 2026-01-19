e22ab35835599ef1fef7426cc2f758be18721485
Marvin0109 <marvin.wendeborn@gmx.de>
2026-01-20 00:26:59 +0100

    Update WebConfig

--- src/main/java/simpleshopapi/config/WebConfig.java
+++ src/main/java/simpleshopapi/config/WebConfig.java
@@ -10,11 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+            .allowedOriginPatterns("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
 
 
