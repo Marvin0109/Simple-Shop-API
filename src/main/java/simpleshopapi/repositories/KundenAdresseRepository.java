676f6a5af91fbeb979740b3c44a8f55b67736b59
Marvin0109 <marvin.wendeborn@gmx.de>
2026-03-28 10:49:06 +0100

    Implementing Repo, Controller and Service for KundenAdresse

--- src/main/java/simpleshopapi/repositories/KundenAdresseRepository.java
+++ src/main/java/simpleshopapi/repositories/KundenAdresseRepository.java
@@ -1,4 +1,41 @@
 package simpleshopapi.repositories;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import simpleshopapi.model.KundenAdresse;
+
+@Repository
 public class KundenAdresseRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public KundenAdresseRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void save(KundenAdresse kundenAdresse) {
+        String sql = "INSERT INTO kunde_hat_adressen (adresse_id, kunde_id, typ) " +
+                "VALUES (?, ?, ?) ON CONFLICT DO NOTHING";
+        jdbcTemplate.update(
+                sql,
+                kundenAdresse.getAdresseId(),
+                kundenAdresse.getKundenId(),
+                kundenAdresse.getTyp()
+        );
+    }
+
+    public int update(String type, KundenAdresse kundenAdresse) {
+        String sql = """
+            UPDATE kunde_hat_adressen
+            SET typ = ?
+            WHERE adresse_id = ? AND kunde_id = ? AND typ = ?
+        """;
+
+        return jdbcTemplate.update(sql, type,
+                kundenAdresse.getAdresseId(),
+                kundenAdresse.getKundenId(),
+                kundenAdresse.getTyp());
+    }
+
+
 }
--- src/main/java/simpleshopapi/service/KundenAdresseService.java
+++ src/main/java/simpleshopapi/service/KundenAdresseService.java
@@ -1,4 +1,33 @@
 package simpleshopapi.service;
 
+import org.springframework.stereotype.Service;
+import simpleshopapi.exception.NotFoundException;
+import simpleshopapi.model.KundenAdresse;
+import simpleshopapi.repositories.KundenAdresseRepository;
+
+@Service
 public class KundenAdresseService {
+
+    private final KundenAdresseRepository repository;
+
+    public KundenAdresseService(KundenAdresseRepository repository) {
+        this.repository = repository;
+    }
+
+    public void create(KundenAdresse kundenAdresse) {
+        repository.save(kundenAdresse);
+    }
+
+    public void updateType(String typ, KundenAdresse kundenAdresse) {
+        if (typ.isBlank()) {
+            throw new IllegalArgumentException("Adresse type is empty");
+        }
+
+        if (!typ.equals(kundenAdresse.getTyp())) {
+            int updated = repository.update(typ, kundenAdresse);
+            if (updated == 0) {
+                throw new NotFoundException("Not able to update type");
+            }
+        }
+    }
 }
